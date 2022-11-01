2679ba0978316979f44baccaa917c5e9cffc3161
diyan santoso <diyan3526@gmail.com>
2022-11-01 09:53:09 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/PRIVATE',],
+  protectedRoutes: ['/PRIVATE',/LOCK'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:diyan3526@gmail.com',
